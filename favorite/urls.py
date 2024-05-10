0d680a8e50e9f68286d72eb30dc144a90cf242c4
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-05-10 19:39:22 +0700

    Update daftar favorite

--- favorite/urls.py
+++ favorite/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from favorite.views import show_favorite
+from favorite.views import show_favorite, show_list_favorite
 
 app_name = 'favorite'
 
 urlpatterns = [
-    path('', show_favorite, name='show_favorite')
+    path('detail/<int:id>/', show_favorite, name='show_favorite'),
+    path('', show_list_favorite , name='show_list_favorite')
 ]
--- favorite/views.py
+++ favorite/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def show_favorite(request):
-    return render(request, 'list_favorite.html')
+def show_favorite(request, id):
+    return render(request, 'list_tayangan_fav.html')
+def show_list_favorite(request):
+    return render(request, 'list_fav.html')
